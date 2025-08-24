8c29c199b44a4b454f147576038cd81ed6c3b795
Manish <mkthakur2301@gmail.com>
2025-08-25 01:17:18 +0530

    Solved P10

---
+++ Solution_S1/P10.java
@@ -0,0 +1,42 @@
+/*
+ * 10. **Finding the Greatest Common Divisor (GCD)**  
+    **Difficulty**: Easy  
+    **Topics**: Basic Programming, Number Theory  
+    **Description**: Write a program to find the GCD of two numbers.  
+    **Example**:  
+    Input: `a = 48, b = 18`  
+    Output: `6`  
+    Explanation: The GCD of 48 and 18 is 6.
+ */
+import java.util.Scanner;
+
+public class P10 {
+
+    static int GCD(int n1, int n2){
+        if(n1%n2==0){
+            return n2;
+        }else{
+            int temp = n1%n2;
+            return GCD(n2, temp);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter n1 number: ");
+        int n1 = scanner.nextInt();
+        System.out.print("Enter n2 number: ");
+        int n2 = scanner.nextInt();
+
+        if (n1<n2){
+            System.out.print(GCD(n2, n1));
+        }else{
+            System.out.print(GCD(n1, n2));
+        }
+        
+        
+        
+        scanner.close();
+    }
+}
